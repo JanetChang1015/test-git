b0158aa478fd80ac90ad83b1e249dbc4ce6f45fc
JanetChang1015 <tchang309@gatech.edu>
2023-09-19 16:01:09 -0400

    Initial Commit

---
+++ test-git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test-git
+//
+//  Created by Janet on 2023/9/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test-git/test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_gitApp.swift
+//  test-git
+//
+//  Created by Janet on 2023/9/19.
+//
+
+import SwiftUI
+
+@main
+struct test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
